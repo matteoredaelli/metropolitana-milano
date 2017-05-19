6989a256f84d28179e17fbec3823dc926938be9e
Matteo <matteo.redaelli@pirelli.com>
2017-05-19 16:49:58 +0200

    linee s

--- setup.sh
+++ setup.sh
@@ -1,1 +1,2 @@
 python3 txt2pl.py < metropolitana-milano.txt | sort > graph.pl
+# curl "http://www.trenord.it/it/circolazione-e-linee/le-linee/linee-s/s13.aspx" | fgrep  -f linee-suburbane.fgrep  | sed -e 's:<br/>:,:' -e 's: - ::'
