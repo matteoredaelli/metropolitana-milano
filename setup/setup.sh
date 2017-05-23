1d7b623bae059505e8068ca40291aea9d55d5f9a
matteoredaelli <matteo.redaelli@gmail.com>
2017-05-23 22:00:55 +0200

    moved files

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,2 +1,2 @@
-cat metr*.txt s*.txt | python3 txt2pl.py | sort > graph.pl
+cat metr*.txt s*.txt | python3 txt2pl.py | sort > ../graph.pl
 # curl "http://www.trenord.it/it/circolazione-e-linee/le-linee/linee-s/s13.aspx" | fgrep  -f linee-suburbane.fgrep  | sed -e 's:<br/>:,:' -e 's: - ::'
